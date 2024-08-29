eb3282a049a819827c5fa0ff35fbae4cbdda7f2b
arccik <arccik@gmail.com>
2024-08-29 07:54:26 +0100

    add meta tag to layout to remove user-scale on ios

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,8 @@ export default function RootLayout({
     <html lang="en">
       <meta
         name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1"
-      ></meta>
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+      />
       <body className="ml-0 sm:ml-20">
         <Providers>
           <ThemeProvider
